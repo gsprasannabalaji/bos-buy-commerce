fc133aeaf3e863e900f84d1699be2c6da8ecb20e
gsprasannabalaji <govindarajalusaras.p@northeastern.edu>
2024-04-10 20:18:42 -0400

    implemented getTopProducts API

--- backend/api/routes/productroute.js
+++ backend/api/routes/productroute.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const productController = require('../controllers/productController');
 
 router.get('/search', productController.searchName);
+router.get('/getTopProducts', productController?.getTopProducts);
 router.get('/:id', productController.search);
-
 
 module.exports = router;
--- backend/api/services/productservice.js
+++ backend/api/services/productservice.js
@@ -1,19 +1,28 @@
-const Product = require('../models/product');
+const Product = require("../models/product");
 
 exports.search = async (id) => {
-    try {
-        const product = await Product.findOne({ productId: id.trim() });;
-        return product; 
-    } catch (err) {
-        throw err; 
-    }
+  try {
+    const product = await Product.findOne({ productId: id.trim() });
+    return product;
+  } catch (err) {
+    throw err;
+  }
 };
 exports.searchByName = async (title) => {
-    try {
-        const regex = new RegExp('.*' + title + '.*', 'i');
-        const product = await Product.find({"productName" : regex});
-        return product; 
-    } catch (err) {
-        throw err; 
-    }
+  try {
+    const regex = new RegExp(".*" + title + ".*", "i");
+    const product = await Product.find({ productName: regex });
+    return product;
+  } catch (err) {
+    throw err;
+  }
+};
+
+exports.getRandomLaptopProducts = async () => {
+  try {
+    const products = await Product.aggregate([{ $sample: { size: 4 } }]);
+    return products;
+  } catch (err) {
+    throw err;
+  }
 };
