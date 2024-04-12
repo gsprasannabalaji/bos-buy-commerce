a2ffa3c6f2a814a38e6aeb16af2eb281dd6efe71
LogeshwaranYogalakshmiSingaravadivelu <yogalakshmisingara.l@northeastern.edu>
2024-04-12 17:48:55 -0400

    Frontend Merged with Background

--- frontend/src/pages/AddProducts.jsx
+++ frontend/src/pages/AddProducts.jsx
@@ -1,6 +1,7 @@
 import { useSelector, useDispatch } from "react-redux";
 import { Container, Form, Button } from "react-bootstrap";
 import { setNewProduct } from "../features/products/productsSlice";
+import axios from "axios";
 
 const AddProducts = () => {
   const newProductData = useSelector((state) => state?.products?.newProduct);
@@ -8,22 +9,51 @@ const AddProducts = () => {
 
   const handleChange = (event) => {
     const { name, value } = event?.target;
-    if(name === "imageURL") {
-        dispatch(setNewProduct({
-            ...newProductData,
-            [name]: event?.target?.files[0]
-        }));
+    if (name === "imageURL") {
+      dispatch(
+        setNewProduct({
+          ...newProductData,
+          [name]: event?.target?.files[0],
+        })
+      );
     } else {
-        dispatch(setNewProduct({
-            ...newProductData,
-            [name]: value
-        }));
+      dispatch(
+        setNewProduct({
+          ...newProductData,
+          [name]: value,
+        })
+      );
     }
   };
 
-  const handleSubmit = (event) => {
-    event?.preventDefault();
-  }
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const formData = new FormData();
+    formData.append("name", event.target.productName.value);
+    formData.append("description", event.target.description.value);
+    formData.append("price", event.target.price.value);
+    formData.append("category", event.target.category.value);
+    if (event.target.imageURL.files[0]) {
+      formData.append("file", event.target.imageURL.files[0]);
+    } else {
+      alert("Please select a file to upload");
+      return;
+    }
+    try {
+      const response = await axios.post(
+        `${import.meta.env.VITE_BACKEND_ENDPOINT_URL}/product/create`,
+        formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        }
+      );
+      window.location.reload();
+    } catch (error) {
+      console.error("Error Posting Data:", error.response.data);
+    }
+  };
 
   return (
     <>
@@ -75,6 +105,18 @@ const AddProducts = () => {
               onChange={handleChange}
               name="price"
               id="price"
+            />
+          </Form.Group>
+
+          <Form.Group htmlFor="description" className="mb-3">
+            <Form.Label>Category</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Enter category"
+              value={newProductData?.category}
+              onChange={handleChange}
+              name="category"
+              id="category"
             />
           </Form.Group>
 
