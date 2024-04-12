4df77b1ce572be5798d16f5e5481ca9cf0fff907
MatheshRameshNEU <ramesh.mat@northeastern.edu>
2024-04-12 17:49:56 -0400

    scss update

--- frontend/src/pages/ProductCategory.jsx
+++ frontend/src/pages/ProductCategory.jsx
@@ -1,16 +1,13 @@
-
-
-import { useNavigate } from 'react-router-dom';
-import { Container, Row, Col, Button, Card } from 'react-bootstrap';
+import { useNavigate } from "react-router-dom";
+import { Container, Row, Col, Button, Card } from "react-bootstrap";
 import laptopcategory from "../assets/laptop_category.webp";
 import desktopcategory from "../assets/desktop_category.webp";
 import gamingcategory from "../assets/gaming_category.webp";
-import '../scss/components/CategoryComponent.scss';
 
 const categories = [
-  { name: 'Laptops', imageUrl: laptopcategory, path: 'laptops' },
-  { name: 'Desktops', imageUrl: desktopcategory, path: 'desktops' },
-  { name: 'Gaming', imageUrl: gamingcategory, path: 'gaming' },
+  { name: "Laptops", imageUrl: laptopcategory, path: "laptops" },
+  { name: "Desktops", imageUrl: desktopcategory, path: "desktops" },
+  { name: "Gaming", imageUrl: gamingcategory, path: "gaming" },
 ];
 
 const CategoryComponent = () => {
@@ -22,23 +19,27 @@ const CategoryComponent = () => {
 
   return (
     <Container className="category-container">
-    <Row className="g-4">
-      {categories.map((category, index) => (
-        <Col key={index} xs={12} md={4} className="mb-4">
-        <Card className="category-card">
-          <Card.Img src={category.imageUrl} alt={category.name} />
-          <Card.ImgOverlay>
-            <Card.Title className="category-title">{category.name}</Card.Title>
-          </Card.ImgOverlay>
-          <Button className="shop-button" onClick={() => navigateToCategory(category.path)}>
-            Shop
-          </Button>
-        </Card>
-      </Col>      
-      ))}
-    </Row>
-  </Container>
-  
+      <Row className="g-4">
+        {categories.map((category, index) => (
+          <Col key={index} xs={12} md={4} className="mb-4">
+            <Card className="category-card">
+              <Card.Img src={category.imageUrl} alt={category.name} />
+              <Card.ImgOverlay>
+                <Card.Title className="category-title">
+                  {category.name}
+                </Card.Title>
+              </Card.ImgOverlay>
+              <Button
+                className="shop-button"
+                onClick={() => navigateToCategory(category.path)}
+              >
+                Shop
+              </Button>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </Container>
   );
 };
 
