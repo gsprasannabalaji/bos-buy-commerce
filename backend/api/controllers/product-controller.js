4d685a170e0cbe1dd9003f934acef27f00d76904
MatheshRameshNEU <ramesh.mat@northeastern.edu>
2024-04-12 15:46:02 -0400

    updated product controller
    updated product controller

--- backend/api/controllers/product-controller.js
+++ backend/api/controllers/product-controller.js
@@ -1,63 +1,70 @@
 const productService = require("../services/product-service");
 
 exports.search = async (req, res) => {
-    try {
-        const { id } = req.params;
-        const product = await productService.search(id);
-        if (product) {
-            res.json(product);
-        } else {
-            res.status(404).send({ message: 'Product not found' });
-        }
-    } catch (err) {
-        console.error(err); 
-        res.status(500).send({ message: 'An error occurred while creating the product' });
+  try {
+    const { id } = req.params;
+    const product = await productService.search(id);
+    if (product) {
+      res.json(product);
+    } else {
+      res.status(404).send({ message: "Product not found" });
     }
+  } catch (err) {
+    console.error(err);
+    res
+      .status(500)
+      .send({ message: "An error occurred while creating the product" });
+  }
 };
 
 exports.create = async (req, res, next) => {
-    try {
-      const uploadResponse = await productService.create(req, res, next);
-      res.json(uploadResponse);
-    } catch (err) {
-      console.error(err);
-      res
-        .status(500)
-        .send({ message: "An error occurred while fetching the product" });
-    }
-  };
-  
+  try {
+    const uploadResponse = await productService.create(req, res, next);
+    res.json(uploadResponse);
+  } catch (err) {
+    console.error(err);
+    res
+      .status(500)
+      .send({ message: "An error occurred while fetching the product" });
+  }
+};
+
 exports.searchName = async (req, res) => {
-    try {
-        const title  = req.query.name;
-        const product = await productService.searchByName(title);
-        if (product) {
-            res.json(product);
-        } else {
-            res.status(404).send({ message: 'No products found' });
-        }
-    } catch (err) {
-        console.error(err); 
-        res.status(500).send({ message: 'An error occurred while fetching the product' });
+  try {
+    const title = req.query.name;
+    const product = await productService.searchByName(title);
+    if (product) {
+      res.json(product);
+    } else {
+      res.status(404).send({ message: "No products found" });
     }
+  } catch (err) {
+    console.error(err);
+    res
+      .status(500)
+      .send({ message: "An error occurred while fetching the product" });
+  }
 };
 
 exports.getTopProducts = async (req, res) => {
-    try {
-        const products = await productService.getRandomLaptopProducts(req, res);
-        res.json(products);
-    } catch (err) {
-        res.status(500).send({ message: 'An error occurred while fetching the product' });
-    }
+  try {
+    const products = await productService.getRandomLaptopProducts(req, res);
+    res.json(products);
+  } catch (err) {
+    res
+      .status(500)
+      .send({ message: "An error occurred while fetching the product" });
+  }
 };
 
-exports.searchCategory=async (req, res) => {
-    try {
-        console.log('Searching category');
-        const category = req.query.category;
-        const products = await productService.searchByCategory(category);
-        res.json(products);
-    } catch (err) {
-        res.status(500).send({ message: 'An error occurred while fetching the product' });
-    }
+exports.searchCategory = async (req, res) => {
+  try {
+    const category = req.query.category;
+    const products = await productService.searchByCategory(category);
+    res.json(products);
+  } catch (err) {
+    res
+      .status(500)
+      .send({ message: "An error occurred while fetching the product" });
+  }
 };
