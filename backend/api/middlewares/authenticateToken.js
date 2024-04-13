fa94bf919b612a292ee54d9ae0865c2c194be063
gsprasannabalaji <govindarajalusaras.p@northeastern.edu>
2024-04-12 21:00:54 -0400

    added the route, service, middleware for login

---
+++ backend/api/middlewares/authenticateToken.js
@@ -0,0 +1,17 @@
+const jwt = require("jsonwebtoken");
+
+const authenticateToken = (req, res, next) => {
+    const token = req.cookies?.["user-creds"];
+    if(token == null) return res.status(401).json({ message: 'User is unauthorized' });
+
+    jwt.verify(token, process.env.JWT_SECRET_KEY, (err, decodeToken) => {
+        if(err) return res.status(403).json({ message: 'Session is Expired! Please login again.' });
+
+        if(req?.url === "/check-admin") {
+            req.userRole = decodeToken?.role;
+        }
+        next();
+    })
+};
+
+module.exports = authenticateToken;
---
+++ backend/api/services/user-service.js
@@ -0,0 +1,40 @@
+const User = require("../models/user");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const login = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User?.findOne({ email: email });
+  if (!user) {
+    return { status: 400, message: "User not found" };
+  }
+  const isMatch = await bcrypt?.compare(password, user?.password);
+  if (!isMatch) {
+    return { status: 400, message: "Invalid password", isUserValid: false };
+  }
+  const token = jwt?.sign(
+    { userName: user?.userName, email: user?.email, role: user?.type },
+    process.env.JWT_SECRET_KEY
+  );
+  res.cookie("user-creds", token, { httpOnly: true });
+  return {
+    isUserValid: isMatch,
+    userName: user?.fullName,
+    role: user?.type,
+  };
+};
+
+const clearCookies = async (req, res) => {
+  res.clearCookie("user-creds");
+  return { message: "cleared the data" };
+};
+
+const isAdminCookie = (role) => {
+    return role === 'admin';
+}
+
+module.exports = {
+    login,
+    clearCookies,
+    isAdminCookie
+};
