8a666cc43f6380ba7877b870651d2aa2f8a46f12
gsprasannabalaji <govindarajalusaras.p@northeastern.edu>
2024-04-13 12:30:32 -0400

    removed the changes

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,6 +4,7 @@ import banner2 from "../assets/banner2.webp";
 import ProductCategory from "../components/ProductCategory";
 import TopProducts from "../components/TopProducts";
 
+
 const Home = () => {
   return (
     <>
