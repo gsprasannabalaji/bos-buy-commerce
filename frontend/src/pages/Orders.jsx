7253f311c27a633e026b6424e6d5ddc3b868bf69
gsprasannabalaji <govindarajalusaras.p@northeastern.edu>
2024-04-14 04:09:52 -0400

    clear the cart items on payment success

--- frontend/src/pages/Orders.jsx
+++ frontend/src/pages/Orders.jsx
@@ -1,4 +1,20 @@
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import { useDispatch } from "react-redux";
+import { clearCart } from '../features/cart/cartSlice';
+
 const Orders = () => {
+    const location = useLocation();
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        const searchParams = new URLSearchParams(location.search);
+        if (searchParams.get('payment') === 'done') {
+          dispatch(clearCart());
+        }
+
+      }, [location.search]);
+
     return (
         <h1>Orders page</h1>
     );
