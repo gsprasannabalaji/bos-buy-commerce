d9c3245f08b30796bd974adf8acf913c15e238ba
LogeshwaranYogalakshmiSingaravadivelu <yogalakshmisingara.l@northeastern.edu>
2024-04-15 14:39:07 -0400

    Navigation

--- frontend/src/common-components/AdminHeader.jsx
+++ frontend/src/common-components/AdminHeader.jsx
@@ -10,7 +10,9 @@ const AdminNavbar = () => {
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
             <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#home">Create</Nav.Link>
+            <LinkContainer to="/addproducts">
+                <Nav.Link>Create</Nav.Link>
+              </LinkContainer>
             <Nav.Link href="#home">AllOrders</Nav.Link>
             <Nav.Link href="#link">Logout</Nav.Link>
           </Nav>
