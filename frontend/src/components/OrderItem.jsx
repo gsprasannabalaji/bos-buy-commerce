08e3ec2035548b1f786df16dab8be93a37358bd8
MatheshRameshNEU <ramesh.mat@northeastern.edu>
2024-04-15 13:26:41 -0400

    updated redux and file structure for order

---
+++ frontend/src/components/OrderItem.jsx
@@ -0,0 +1,51 @@
+// OrderItem.js
+import React from 'react';
+import { Card, ListGroup, Row, Col } from 'react-bootstrap';
+import ProductItem from './ProductItem'; // Import ProductItem component
+
+const OrderItem = ({ order }) => {
+  const totalPrice = order.totalPrice;
+  const date = new Date(order.date).toLocaleDateString();
+
+  return (
+    <Card className="mb-4 shadow-sm">
+      <Card.Header className="bg-grey">
+        <Row className="align-items-center">
+          <Col>
+            <strong>Order Placed:</strong> {date}
+          </Col>
+          <Col>
+            <strong>Order#:</strong> {order.orderId}
+          </Col>
+          <Col className="text-right">
+            <strong>Total:</strong> ${totalPrice}
+          </Col>
+        </Row>
+      </Card.Header>
+      <ListGroup className="list-group-flush">
+        {order.products.map((product, index) => (
+          <ListGroup.Item key={product._id.$oid}>
+            <ProductItem product={product} />
+          </ListGroup.Item>
+        ))}
+        <ListGroup.Item>
+          <Row>
+            <Col md={{ span: 10, offset: 2 }}>
+              <div>
+                <strong>Delivery Address:</strong>
+                {} {order.shippingAddress.line1},{order.shippingAddress.line2},
+                {order.shippingAddress.city},{order.shippingAddress.state},
+                {order.shippingAddress.postal_code}
+              </div>
+              <div>
+                <strong>Fulfilled by BOSbuy</strong>
+              </div>
+            </Col>
+          </Row>
+        </ListGroup.Item>
+      </ListGroup>
+    </Card>
+  );
+};
+
+export default OrderItem;
--- frontend/src/pages/Orders.jsx
+++ frontend/src/pages/Orders.jsx
@@ -1,85 +1,17 @@
-import React, { useEffect, useState } from "react";
+// Orders.js
+import { useEffect } from "react";
 import axios from "axios";
 import { useLocation } from "react-router-dom";
-import { useDispatch } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { clearCart } from "../features/cart/cartSlice";
-import { Card, ListGroup, Row, Col, Image } from "react-bootstrap";
-import { Link } from "react-router-dom";
-
-const ProductItem = ({ product }) => {
-  const price = product.price;
-  const quantity = product.quantity;
-
-  return (
-    <Row className="mb-3">
-      <Col md={2}>
-        <Link to={`/products/${product.productId}`}>
-          <Image src={product.imageURL} fluid />
-        </Link>
-      </Col>
-      <Col md={10}>
-        <Link to={`/products/${product.productId}`}>
-          <div>
-            <strong>{product.productName}</strong>
-          </div>
-        </Link>
-        <div>Qty : {quantity}</div>
-        <div>Price : ${price}</div>
-      </Col>
-    </Row>
-  );
-};
-
-const OrderItem = ({ order }) => {
-  const totalPrice = order.totalPrice;
-  const date = new Date(order.date).toLocaleDateString();
-
-  return (
-    <Card className="mb-4 shadow-sm">
-      <Card.Header className="bg-grey">
-        <Row className="align-items-center">
-          <Col>
-            <strong>Order Placed:</strong> {date}
-          </Col>
-          <Col>
-            <strong>Order#:</strong> {order.orderId}
-          </Col>
-          <Col className="text-right">
-            <strong>Total:</strong> ${totalPrice}
-          </Col>
-        </Row>
-      </Card.Header>
-      <ListGroup className="list-group-flush">
-        {order.products.map((product, index) => (
-          <ListGroup.Item key={product._id.$oid}>
-            <ProductItem product={product} />
-          </ListGroup.Item>
-        ))}
-        <ListGroup.Item>
-          <Row>
-            <Col md={{ span: 10, offset: 2 }}>
-              <div>
-                <strong>Delivery Address:</strong>
-                {} {order.shippingAddress.line1},{order.shippingAddress.line2},
-                {order.shippingAddress.city},{order.shippingAddress.state},
-                {order.shippingAddress.postal_code}
-              </div>
-              <div>
-                {" "}
-                <strong>Fulfilled by BOSbuy</strong>
-              </div>
-            </Col>
-          </Row>
-        </ListGroup.Item>
-      </ListGroup>
-    </Card>
-  );
-};
+import OrderItem from '../components/OrderItem'; // Make sure this path is correct
+import { Container } from "react-bootstrap";
+import { setOrders } from "../features/orders/ordersSlice";
 
 const Orders = () => {
+  const orders = useSelector((state) => state?.orders?.orders);
   const location = useLocation();
   const dispatch = useDispatch();
-  const [orders, setOrders] = useState([]);
 
   useEffect(() => {
     const fetchOrders = async () => {
@@ -109,26 +41,26 @@ const Orders = () => {
             ...order.shippingAddress,
           },
         }));
-        setOrders(ordersData);
+        dispatch(setOrders(ordersData));
       } catch (error) {
         console.error("Error fetching orders:", error);
       }
     };
 
     fetchOrders();
-  }, []);
+  }, [location.search, dispatch]);
 
   return (
-    <div className="container">
+    <Container className="container">
       <h1>Your Orders</h1>
       {orders.length > 0 ? (
-        orders.map((order, index) => (
+        orders.map((order) => (
           <OrderItem key={order._id.$oid} order={order} />
         ))
       ) : (
         <p>No orders found.</p>
       )}
-    </div>
+    </Container>
   );
 };
 
