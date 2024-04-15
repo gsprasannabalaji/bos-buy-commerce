5ee208d75de593cfc9b763c8d8cd6a6537789817
LogeshwaranYogalakshmiSingaravadivelu <yogalakshmisingara.l@northeastern.edu>
2024-04-15 14:55:10 -0400

    Added Routing

--- frontend/src/common-components/AdminHeader.jsx
+++ frontend/src/common-components/AdminHeader.jsx
@@ -9,12 +9,10 @@ const AdminNavbar = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <LinkContainer to="/addproducts">
-                <Nav.Link>Create</Nav.Link>
-              </LinkContainer>
-            <Nav.Link href="#home">AllOrders</Nav.Link>
-            <Nav.Link href="#link">Logout</Nav.Link>
+            <Nav.Link href="/admin">Home</Nav.Link>
+            <Nav.Link href="/addproducts">Create</Nav.Link>
+            <Nav.Link href="/allorders">AllOrders</Nav.Link>
+            <Nav.Link href="/login">Logout</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
