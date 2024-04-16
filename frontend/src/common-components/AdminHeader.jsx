c709be77ca1a8a601071e19b724ae73cb3000ae1
gsprasannabalaji <govindarajalusaras.p@northeastern.edu>
2024-04-15 23:53:40 -0400

    updated the styles

--- frontend/src/common-components/AdminHeader.jsx
+++ frontend/src/common-components/AdminHeader.jsx
@@ -4,29 +4,12 @@ import { Navbar, Nav, Container } from "react-bootstrap";
 const AdminHeader = () => {
   return (
     <>
-      <style>
-        {`
-          @media (min-width: 993px) {
-            /* Hides Nav when sidebar is visible */
-            .admin-nav {
-              display: none;
-            }
-          }
-
-          @media (max-width: 992px) {
-            /* Shows Nav when sidebar is hidden */
-            .admin-nav {
-              display: flex;
-            }
-          }
-        `}
-      </style>
       <Navbar bg="dark" variant="dark" expand="lg" className="admin-header">
         <Container fluid>
           <Navbar.Brand href="#home">Admin Dashboard</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ms-auto admin-nav">
+            <Nav className="ms-auto d-flex d-lg-none">
               <Nav.Link href="/admin">Home</Nav.Link>
               <Nav.Link href="/addproducts">Create</Nav.Link>
               <Nav.Link href="/allorders">All Orders</Nav.Link>
