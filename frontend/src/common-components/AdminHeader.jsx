c95c6822b23f080470de01055385962f44f02721
LogeshwaranYogalakshmiSingaravadivelu <yogalakshmisingara.l@northeastern.edu>
2024-04-16 13:02:48 -0400

    Added Logout in Admin Header

--- frontend/src/common-components/AdminHeader.jsx
+++ frontend/src/common-components/AdminHeader.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
+import axios from "axios";
 
 const AdminHeader = () => {
+  const handleLogOut = async () => {
+    try {
+      await axios.get(
+        `${import.meta.env.VITE_BACKEND_ENDPOINT_URL}/user/clearCookies`,
+        { withCredentials: true }
+      );
+      localStorage.removeItem("userDetails");
+      window.location.reload();
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <>
       <Navbar bg="dark" variant="dark" expand="lg" className="admin-header">
@@ -13,7 +27,7 @@ const AdminHeader = () => {
               <Nav.Link href="/admin">Home</Nav.Link>
               <Nav.Link href="/addproducts">Create</Nav.Link>
               <Nav.Link href="/allorders">All Orders</Nav.Link>
-              <Nav.Link href="/login">Logout</Nav.Link>
+              <Nav.Link onClick={handleLogOut}>Logout</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
