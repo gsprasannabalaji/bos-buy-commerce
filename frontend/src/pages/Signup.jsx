99ec8c57f18d920b8ac6df9641f56d6ac1befe56
gsprasannabalaji <govindarajalusaras.p@northeastern.edu>
2024-04-15 20:42:34 -0400

    updated the styles for signup

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -100,90 +100,96 @@ const SignUp = () => {
         <Loader />
       ) : (
         <div className="signup-page">
-          <Row className="justify-content-center">
-            <Col sm={12} lg={6} className="graphic-section px-0">
-              <img src={banner2} alt="Sign Up Visual" className="" />
-            </Col>
-            <Col sm={12} lg={6} className="signup-form-section ">
-              <Form onSubmit={handleSubmit}>
-                <h3>Create Account</h3>
+          <Container>
+            <Row className="flex-column-reverse flex-lg-row">
+              <Col xs={12} lg={6} className="signup-page__image-container px-0">
+                <img
+                  src={banner2}
+                  alt="Sign Up Visual"
+                  className="signup-page__image-container__img"
+                />
+              </Col>
+              <Col xs={12} lg={6} className="signup-form-section">
+                <Form onSubmit={handleSubmit}>
+                  <h3>Create Account</h3>
 
-                <Form.Group className="mb-3" controlId="formBasicUsername">
-                  <Form.Label>User Name</Form.Label>
-                  <Form.Control
-                    type="text"
-                    placeholder="User Name"
-                    name="username"
-                    value={formData.username}
-                    onChange={handleChange}
-                    isInvalid={!!errors.username}
-                    required
-                  />
-                  <Form.Control.Feedback type="invalid">
-                    {errors.username}
-                  </Form.Control.Feedback>
-                </Form.Group>
+                  <Form.Group className="mb-3" controlId="formBasicUsername">
+                    <Form.Label>User Name</Form.Label>
+                    <Form.Control
+                      type="text"
+                      placeholder="User Name"
+                      name="username"
+                      value={formData.username}
+                      onChange={handleChange}
+                      isInvalid={!!errors.username}
+                      required
+                    />
+                    <Form.Control.Feedback type="invalid">
+                      {errors.username}
+                    </Form.Control.Feedback>
+                  </Form.Group>
 
-                <Form.Group className="mb-3" controlId="formBasicEmail">
-                  <Form.Label>Email address</Form.Label>
-                  <Form.Control
-                    type="email"
-                    placeholder="Enter email"
-                    name="email"
-                    value={formData.email}
-                    onChange={handleChange}
-                    isInvalid={!!errors.email}
-                    required
-                  />
-                  <Form.Control.Feedback type="invalid">
-                    {errors.email}
-                  </Form.Control.Feedback>
-                </Form.Group>
+                  <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control
+                      type="email"
+                      placeholder="Enter email"
+                      name="email"
+                      value={formData.email}
+                      onChange={handleChange}
+                      isInvalid={!!errors.email}
+                      required
+                    />
+                    <Form.Control.Feedback type="invalid">
+                      {errors.email}
+                    </Form.Control.Feedback>
+                  </Form.Group>
 
-                <Form.Group className="mb-3" controlId="formBasicPassword">
-                  <Form.Label>Password</Form.Label>
-                  <Form.Control
-                    type="password"
-                    placeholder="Password"
-                    name="password"
-                    value={formData.password}
-                    onChange={handleChange}
-                    isInvalid={!!errors.password}
-                    required
-                  />
-                  <Form.Control.Feedback type="invalid">
-                    {errors.password}
-                  </Form.Control.Feedback>
-                </Form.Group>
+                  <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control
+                      type="password"
+                      placeholder="Password"
+                      name="password"
+                      value={formData.password}
+                      onChange={handleChange}
+                      isInvalid={!!errors.password}
+                      required
+                    />
+                    <Form.Control.Feedback type="invalid">
+                      {errors.password}
+                    </Form.Control.Feedback>
+                  </Form.Group>
 
-                <Form.Group
-                  className="mb-3"
-                  controlId="formBasicConfirmPassword"
-                >
-                  <Form.Label>Confirm Password</Form.Label>
-                  <Form.Control
-                    type="password"
-                    placeholder="Confirm Password"
-                    name="confirmPassword"
-                    value={formData.confirmPassword}
-                    onChange={handleChange}
-                    isInvalid={!!errors.confirmPassword}
-                    required
-                  />
-                  <Form.Control.Feedback type="invalid">
-                    {errors.confirmPassword}
-                  </Form.Control.Feedback>
-                </Form.Group>
+                  <Form.Group
+                    className="mb-3"
+                    controlId="formBasicConfirmPassword"
+                  >
+                    <Form.Label>Confirm Password</Form.Label>
+                    <Form.Control
+                      type="password"
+                      placeholder="Confirm Password"
+                      name="confirmPassword"
+                      value={formData.confirmPassword}
+                      onChange={handleChange}
+                      isInvalid={!!errors.confirmPassword}
+                      required
+                    />
+                    <Form.Control.Feedback type="invalid">
+                      {errors.confirmPassword}
+                    </Form.Control.Feedback>
+                  </Form.Group>
 
-                <Button variant="warning" type="submit" className="w-100">
-                  Create your account
-                </Button>
-                <div className="mt-3 text-center">
-                  Already have an account? <a href="/login">Sign in</a>
-                </div>
-              </Form>
-            </Col>
-          </Row>
+                  <Button variant="warning" type="submit" className="w-100">
+                    Create your account
+                  </Button>
+                  <div className="mt-3 text-center">
+                    Already have an account? <a href="/login">Sign in</a>
+                  </div>
+                </Form>
+              </Col>
+            </Row>
+          </Container>
         </div>
       )}
     </>
